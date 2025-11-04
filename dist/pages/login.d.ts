e73dcaa215604da38833e48182c2d4fccc565c4f
Diwix6 <102622118+Diwix6@users.noreply.github.com>
2025-11-04 05:20:39 +0300

    Delete dist directory

--- dist/pages/login.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Рендерит страницу входа пользователя
- * @returns {HTMLElement} DOM-элемент страницы входа
- */
-export declare function renderLogin(app: HTMLElement): void;
--- dist/pages/notes.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Рендерит страницу заметок с секциями избранных и недавних заметок
- * @param {Object} [params] ъ
- * @param {Array<Object>} [params.notes=[]] - Массив объектов заметок для отображения
- * @returns {HTMLElement} DOM-элемент страницы заметок
- */
-export declare function renderNotes(app: HTMLElement): void;
