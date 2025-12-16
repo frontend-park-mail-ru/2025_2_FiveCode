2bcd82e1c3dbc2e9e00434cd433d0672d0164837
andrey57x <57andreys57@gmail.com>
2025-12-16 19:05:06 +0300

    lintex fix

--- src/app.ts
+++ src/app.ts
@@ -98,7 +98,7 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     } else {
       navigator.serviceWorker.getRegistrations().then((registrations) => {
-        for (let registration of registrations) {
+        for (const registration of registrations) {
           registration.unregister();
         }
       });
